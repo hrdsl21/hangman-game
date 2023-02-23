2704a788465ffff932157b01749948f359730100
hrdsl21 <121196800+hrdsl21@users.noreply.github.com>
2023-02-23 09:26:57 +0100

    Update python.py

--- python.py
+++ python.py
@@ -1,48 +1,36 @@
 import random
+import requests
+import os
 
-#lista haseł
-lista = ['Mountain', 'horse', 'house', 'dog', 'hamster']
-
-#liczba żyć
+url = "https://random-word-api.herokuapp.com/word"
+random_password = requests.get(url).json()[0]
 lifs = 6 
-#generator hasła
-random_password = random.choice(lista)
 list_gen = list(random_password)
-print(list_gen)
+list_gen = list("_"*len(random_password))
 
 print("You start the hangman game")
 
-#Generator _____
-for i in range(len(random_password)):
-    list_gen[i] = "_"
-
-
 while lifs > 0:
-    print(f"You have left {lifs} lifs ")
-    print("")
-    print(f"Password : {list_gen}")
-    print("")
+    os.system('cls')
+    print(random_password, end="\n\n")
+    print(f"You have left {lifs} lifs\n\n")
+    print(f"Password : {list_gen}\n\n")
 
-    answer = input("Enter the first letter for a word from the list")
+    answer = input("Enter the first letter for a word from the list: ")
     if answer in random_password:
-        #sprawdzamy czy i podmieniamy znaki na _
+        
         for i in range(len(random_password)):
             if (random_password[i]) == answer:
                 list_gen[i] = answer
                 print(list_gen)
-            #sprawdzenie czy wygrałes   
+              
             if list_gen == list(random_password):
-                print("")
-                print(f"You have left {lifs} lifs \n\nPassword :{random_password}\n\nYOU WIN")
+                print(f"\nYou have left {lifs} lifs \n\nPassword :{random_password}\n\nYOU WIN")
                 exit()
     else:
         lifs -= 1 
         if lifs == 0:
-            print('Now you will give your life on the gallows')
-            print('Try again')
-        
-
-
+            print('Now you will give your life on the gallows\nTry again')        
     
 
 
